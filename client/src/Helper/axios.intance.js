ac1bfec111f4cb263b5add139ec58c028b514b8a
rohan <khem7869218659@gmail.com>
2025-07-30 22:49:42 +0530

    env add frontend

--- client/src/Helper/axios.intance.js
+++ client/src/Helper/axios.intance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { config } from 'dotenv';
 
-const BASE_URL ="http://localhost:5000/api/v1";
+const BASE_URL =import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create();
 
